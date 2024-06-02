527e32e7769112ef57bf5b3d929b5b076d618481
safarov-en <safarov.elkhan110@gmail.com>
2024-06-02 12:15:54 +0500

    Adding an alert

---
+++ src/components/request/RequestModal.vue
@@ -0,0 +1,8 @@
+<template>
+    <form>
+        <div class="form-control">
+            <label for="">ФИО</label>
+            <input type="text">
+        </div>
+    </form>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="home">
-  </div>
+  <app-page title="Список заявок">
+    <template #header>
+      <button class="btn primary" @click="modal = true">Создать</button>
+    </template>
+    <request-table :requests="[]"></request-table>
+    <teleport to="body">
+      <app-modal v-if="modal" title="Создать заявку" @close="modal = false">
+        <request-modal/>
+      </app-modal>
+    </teleport>
+  </app-page>
 </template>
 
 <script>
+import AppPage from '@/components/ui/AppPage.vue';
+import RequestTable from '@/components/request/RequestTable.vue';
+import AppModal from '@/components/ui/AppModal.vue';
+import RequestModal from '../components/request/RequestModal.vue'
+import { ref } from 'vue';
 export default {
-  name: 'Home',
+  components: {AppPage, RequestTable, AppModal, RequestModal},
+  setup() {
+    const modal = ref(false)
+    return {
+      modal
+    }
+  }
 }
 </script>
