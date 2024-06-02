5e5aab0261b81f714c12c0b94978d27e2cdd092a
safarov-en <safarov.elkhan110@gmail.com>
2024-06-02 14:53:26 +0500

    Alert validation

--- src/components/request/RequestModal.vue
+++ src/components/request/RequestModal.vue
@@ -1,8 +1,43 @@
 <template>
-    <form>
-        <div class="form-control">
-            <label for="">ФИО</label>
-            <input type="text">
+    <form @submit.prevent="onSumbit">
+        <div class="form-control" :class="{invalid:fError}">
+            <label for="fio">ФИО</label>
+            <input type="text" id="fio" v-model="fio" @blur="fBlur">
+            <small v-if="fError">{{ fError }}</small>
         </div>
+        <div class="form-control" :class="{invalid:pError}">
+            <label for="phone">Телефон</label>
+            <input type="text" id="phone" v-model="phone" @blur="pBlur">
+            <small v-if="pError">{{ pError }}</small>
+        </div>
+        <div class="form-control" :class="{invalid:aError}">
+            <label for="amount">Сумма</label>
+            <input type="text" id="amount" v-model.number="amount" @blur="ablur">
+            <small v-if="aError">{{ aError }}</small>
+        </div>
+        <div class="form-control">
+            <label for="status">Статус</label>
+            <select id="status" v-model="status">
+                <option value="done">Завершен</option>
+                <option value="cancelled">Отменен</option>
+                <option value="active">Активен</option>
+                <option value="pending">Выполняется</option>
+            </select>
+        </div>
+        <button class="btn primary" :disabled="isSubmitting">Создать</button>
     </form>
 </template>
+<script>
+import {useRequestForm} from '../../use/request-form.js'
+export default {
+    emits: ['created'],
+    setup(_, {emit}) {
+        const submit = async values => {
+            emit('created')
+        }
+        return {
+            ...useRequestForm(submit)
+        }
+    }
+}
+</script>
