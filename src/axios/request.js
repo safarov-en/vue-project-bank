0edfeb3b8c9bfcdd8d85b991791f8c5e60c6ea28
safarov-en <safarov.elkhan110@gmail.com>
2024-06-03 16:19:53 +0500

    Output of elements

--- src/axios/request.js
+++ src/axios/request.js
@@ -1,7 +1,15 @@
+import router from "@/router";
 import axios from "axios";
 
 const requestAxios = axios.create({
     baseURL: process.env.VUE_APP_FB_URL
 })
+
+requestAxios.interceptors.response.use(null, (error) => {
+    if(error.response.status === 401) {
+        router.push('/auth?message=auth')
+    }
+    return Promise.reject(error);
+});
 
 export default requestAxios
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
-  <app-page title="Список заявок">
+  <app-loader v-if="loading"/>
+  <app-page title="Список заявок" v-else>
     <template #header>
       <button class="btn primary" @click="modal = true">Создать</button>
     </template>
@@ -17,17 +18,25 @@ import AppPage from '@/components/ui/AppPage.vue';
 import RequestTable from '@/components/request/RequestTable.vue';
 import AppModal from '@/components/ui/AppModal.vue';
 import RequestModal from '../components/request/RequestModal.vue'
-import { computed, ref } from 'vue';
+import { computed, ref, onMounted } from 'vue';
 import { useStore } from 'vuex';
+import AppLoader from '../components/ui/AppLoader'
 export default {
-  components: {AppPage, RequestTable, AppModal, RequestModal},
+  components: {AppPage, RequestTable, AppModal, RequestModal, AppLoader},
   setup() {
     const store = useStore()
     const modal = ref(false)
+    const loading = ref(false)
+    onMounted(async () => {
+      loading.value = true
+      await store.dispatch('request/load')
+      loading.value = false
+    })
     const requests = computed(() => store.getters['request/requests'])
     return {
       modal,
-      requests
+      requests,
+      loading
     }
   }
 }
