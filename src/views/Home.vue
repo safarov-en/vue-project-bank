4c558791cbb2273e72d6427dcbe53ea0b7f5a9a9
safarov-en <safarov.elkhan110@gmail.com>
2024-06-04 18:35:30 +0500

    Filtering elements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
     <template #header>
       <button class="btn primary" @click="modal = true">Создать</button>
     </template>
+    <request-filter v-model="filter"></request-filter>
     <request-table :requests="requests"></request-table>
     <teleport to="body">
       <app-modal v-if="modal" title="Создать заявку" @close="modal = false">
@@ -21,22 +22,38 @@ import RequestModal from '../components/request/RequestModal.vue'
 import { computed, ref, onMounted } from 'vue';
 import { useStore } from 'vuex';
 import AppLoader from '../components/ui/AppLoader'
+import RequestFilter from '@/components/request/RequestFilter.vue';
 export default {
-  components: {AppPage, RequestTable, AppModal, RequestModal, AppLoader},
+  components: {AppPage, RequestTable, AppModal, RequestModal, AppLoader, RequestFilter},
   setup() {
     const store = useStore()
     const modal = ref(false)
     const loading = ref(false)
+    const filter = ref({})
     onMounted(async () => {
       loading.value = true
       await store.dispatch('request/load')
       loading.value = false
     })
-    const requests = computed(() => store.getters['request/requests'])
+    const requests = computed(() => store.getters['request/requests']
+      .filter(request => {
+        if(filter.value.name) {
+          return request.fio.includes(filter.value.name)
+        }
+        return request
+      })
+      .filter(request => {
+        if(filter.value.status) {
+          return filter.value.status === request.status
+        }
+        return request
+      })
+    )
     return {
       modal,
       requests,
-      loading
+      loading,
+      filter
     }
   }
 }
