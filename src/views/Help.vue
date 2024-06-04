4d46622869b2fecabb7494f9784b2cbbddad66f4
safarov-en <safarov.elkhan110@gmail.com>
2024-06-04 20:59:52 +0500

    Adding a help page

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,5 +1,23 @@
 <template>
-    <div class="about">
-        <h1>This is an about page</h1>
-    </div>
+    <app-page back title="Помощь">
+        <question-item
+            v-for="q in questions"
+            :key="q.title"
+            :question="q"
+        ></question-item>
+    </app-page>
 </template>
+<script>
+import AppPage from '../components/ui/AppPage'
+import QuestionItem from '../components/question/QuestionItem'
+export default {
+    setup() {
+        const questions = [
+            {title: 'Что есть в данной системе?', text: 'Тут есть все, для того, чтобы смотреть, какие заявки человек добавлял'},
+            {title: 'Как тут работает авторизация?', text: 'Все реализовано через Firebase'}
+        ]
+        return {questions}
+    },
+    components: {AppPage, QuestionItem}
+}
+</script>
